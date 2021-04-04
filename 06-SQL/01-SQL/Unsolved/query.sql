df7bd709bb056621ffb0fcccb4ee02b4afc1c9c4
Ruthicaf1 <77359177+Ruthicaf1@users.noreply.github.com>
2021-04-04 17:26:43 -0400

    Add files via upload

--- 06-SQL/01-SQL/Unsolved/query.sql
+++ 06-SQL/01-SQL/Unsolved/query.sql
@@ -15,7 +15,6 @@ select retiring.emp_no,
 
 
 -- Select only the current title for each employee	
-
 with emp_fil as (
 with retiring as (select * from employees 
 where to_char(birth_date, 'YYYY-MM-DD') between '1952-01-01' and '1955-12-31')
@@ -34,10 +33,11 @@ select retiring.emp_no,
 cur_emp as (
 select emp_no, max(from_date) as most_recent from titles group by emp_no)
 
-select emp_fil.emp_no, emp_fil.first_name, emp_fil.last_name, emp_fil.title as current_title from emp_fil join cur_emp on ((emp_fil.emp_no=cur_emp.emp_no) and (emp_fil.from_date=cur_emp.most_recent));
+select emp_fil.emp_no, emp_fil.first_name, emp_fil.last_name, emp_fil.title as cur_title from emp_fil join cur_emp on ((emp_fil.emp_no=cur_emp.emp_no) and (emp_fil.from_date=cur_emp.most_recent));
 
 
--- Count the total number of employees about to retire by their current job title
+--Count the total number of employees about to retire by their current job title
+
 with lat_title as (
 with emp_fil as (
 with retiring as (select * from employees 
@@ -57,16 +57,16 @@ select retiring.emp_no,
 cur_emp as (
 select emp_no, max(from_date) as most_recent from titles group by emp_no)
 
-select emp_fil.emp_no, emp_fil.first_name, emp_fil.last_name, emp_fil.title as current_title from emp_fil join cur_emp on ((emp_fil.emp_no=cur_emp.emp_no) and (emp_fil.from_date=cur_emp.most_recent)))
+select emp_fil.emp_no, emp_fil.first_name, emp_fil.last_name, emp_fil.title as cur_title from emp_fil join cur_emp on ((emp_fil.emp_no=cur_emp.emp_no) and (emp_fil.from_date=cur_emp.most_recent)))
 
-select current_title, count (*) as employee_count from lat_title group by current_title;
+select cur_title, count (*) as employee_count from lat_title group by cur_title;
 
 
 -- Count the total number of employees per department
 
 with count_by_dept as(
-select dept_no, count(emp_no) as num_of_employee from dept_emp group by dept_no )
-select dept_name, count_by_dept.num_of_employee from departments join count_by_dept on (departments.dept_no=count_by_dept.dept_no);
+select dept_no, count(emp_no) as num_of_employee_per_dept from dept_emp group by dept_no )
+select dept_name, count_by_dept.num_of_employee_per_dept from departments join count_by_dept on (departments.dept_no=count_by_dept.dept_no);
 
 
 -- Bonus: Find the highest salary per department and department manager
